2885ff37c984ca9fb3f5dce4ad86dc44bb977a92
shinyeji28 <xntm10@naver.com>
2022-04-26 16:18:28 +0900

    routing by function module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
-import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { HeroListComponent } from './heroes/hero-list/hero-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes : Routes = [
   { path: 'crisis-center', component: CrisisListComponent },
-  { path: 'heroes',        component: HeroesListComponent },
+  { path: 'heroes',        component: HeroListComponent },
   {path: '', redirectTo: '/heroes', pathMatch: 'full'}, // 빈 주소 접근시 리다이렉트 path 정의 / pathMatch:full -> 주소가 정확히 ''일때만 매칭되어 라우팅 적용
   {path: '**', component: PageNotFoundComponent}  // 정의하지 않은 주소로 접근시 (와일드카드 라우팅- 라우팅 규칙 마지막에 등록) 
 ]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,28 +5,22 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
-import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { HeroListComponent } from './heroes/hero-list/hero-list.component';
 
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { applySourceSpanToExpressionIfNeeded } from '@angular/compiler/src/output/output_ast';
-
-const appRoutes : Routes = [
-  { path: 'crisis-center', component: CrisisListComponent },
-  { path: 'heroes',        component: HeroesListComponent },
-  {path: '', redirectTo: '/heroes', pathMatch: 'full'}, // 빈 주소 접근시 리다이렉트 path 정의 / pathMatch:full -> 주소가 정확히 ''일때만 매칭되어 라우팅 적용
-  {path: '**', component: PageNotFoundComponent}  // 정의하지 않은 주소로 접근시 (와일드카드 라우팅- 라우팅 규칙 마지막에 등록) 
-]
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     CrisisListComponent,
-    HeroesListComponent,
     PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
+    HeroesModule,
     AppRoutingModule
   ],
   providers: [],
